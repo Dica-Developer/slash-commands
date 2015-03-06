100ccbd28276dc9e5d931b96b186ca53ba78d632
Martin Schaaf <mschaaf@datameer.com>
2015-03-06 09:08:33 +0100

    * rename from index.js to server.js * remove getting started occurrences

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+var Url = require('url');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/wetter', function(request, response) {
+  var url = Url.parse(request.url);
+  console.log(url.query);
+  response.send(url.query);
+  // http://api.openweathermap.org/data/2.5/weather?q=
 });
 
 app.listen(app.get('port'), function() {
