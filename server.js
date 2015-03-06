a511cfd5cc9fd983aa1c691a4b3893e9e6442425
Martin Schaaf <mschaaf@datameer.com>
2015-03-06 10:42:26 +0100

    * handle errors * return the first weather description found

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 var Url = require('url');
-var express = require('express');
 var http = require('http');
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/wetter', function(request, response) {
-  var url = Url.parse(request.url);
-  if (url.query) {
-    var query = url.query;
-    if (query.hasOwnProperty('text')) {
-      http.get('https://api.openweathermap.org/data/2.5/weather?q=' + query.text, function(weatherResponse) {
-        response.send(weatherResponse);
-      }).on('error', function() {
-        response.send('No weather for you today!!!');
+  var url = Url.parse(request.url, true);
+  if (null !== url.query && undefined !== url.query && url.query.hasOwnProperty('text')) {
+    http.get('http://api.openweathermap.org/data/2.5/weather?q=' + url.query.text, function(weatherResponse) {
+      var body = '';
+      weatherResponse.on('data', function(chunk) {
+        body = body + chunk;
       });
-    }
+      weatherResponse.on('end', function(chunk) {
+        var weatherDescription = JSON.parse(body).weather[0].description;
+        response.send('/giphy ' + weatherDescription);
+      });
+    }).on('error', function() {
+      response.send('No weather for you today.');
+    });
+  } else {
+    response.send('Help me please! Weather is a local phenomenon so specify a city!!!');
   }
 });
 
